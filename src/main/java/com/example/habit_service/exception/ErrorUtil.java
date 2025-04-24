cfb85839798da91bfd690f7929671ca3f2e12f30
max <mwlosev@gmail.com>
2025-04-24 18:33:25 +0300

    Added processing of ValidationException and Exception

--- src/main/java/com/example/habit_service/exception/ErrorUtil.java
+++ src/main/java/com/example/habit_service/exception/ErrorUtil.java
@@ -1,4 +1,21 @@
 package com.example.habit_service.exception;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
 public class ErrorUtil {
+    public static void throwIfHasErrors(BindingResult bindingResult) {
+        StringBuilder errorMsg = new StringBuilder();
+
+        List<FieldError> errors = bindingResult.getFieldErrors();
+        for (FieldError error : errors) {
+            errorMsg.append(error.getField())
+                    .append(" - ").append(error.getDefaultMessage() == null ? error.getCode() : error.getDefaultMessage())
+                    .append(";");
+        }
+
+        throw new ValidationException(errorMsg.toString());
+    }
 }
--- src/main/java/com/example/habit_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/habit_service/exception/GlobalExceptionHandler.java
@@ -1,4 +1,25 @@
 package com.example.habit_service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handleValidationException(ValidationException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", e.getMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleAnyException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("error", "Internal server error"));
+    }
 }
