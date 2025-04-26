246fe0ced956f66157cb6e0419ba2b234d2ad6dc
max <mwlosev@gmail.com>
2025-04-26 11:49:47 +0300

    added configuration for OpenAPI and CSRF

--- src/main/java/com/example/habit_service/config/HabitConfig.java
+++ src/main/java/com/example/habit_service/config/HabitConfig.java
@@ -3,9 +3,38 @@ package com.example.habit_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class HabitConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // разрешить preflight OPTIONS
+                        .requestMatchers(HttpMethod.GET, "/habits", "/habits/{id}").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/habits").permitAll()
+                        .requestMatchers(HttpMethod.PATCH, "/habits/{id}").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/habits/{id}").permitAll()
+                )
+                .build();
+    }
+
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
